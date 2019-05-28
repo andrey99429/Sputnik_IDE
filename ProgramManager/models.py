a91e75682592634692770af0a2a1008321959a0c
Andrey Popov <a.popov99@mail.ru>
2019-05-28 17:16:55 +0300

    Created models and made migrations

--- ProgramManager/models.py
+++ ProgramManager/models.py
@@ -1,8 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class Program(models.Model):
-    name = models.CharField(max_length=150)
-    dir_path = models.CharField(null=True, max_length=50)
-    exec_path = models.CharField(null=True, max_length=50)
+class Project(models.Model):
+    name = models.CharField(max_length=50)
+    dir_path = models.CharField(max_length=150)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+
+
+class Version(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.PROTECT)
+    dir_path = models.CharField(max_length=200)
+    make_name = models.CharField(max_length=50)
+    exec_name = models.CharField(null=True, max_length=50)
     upload_time = models.DateTimeField()
+    compile_time = models.DateTimeField(null=True)
