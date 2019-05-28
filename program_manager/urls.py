db097a866b9a3c43e4d6884a9b18fd9ae95d094c
Andrey Popov <a.popov99@mail.ru>
2019-05-28 17:18:59 +0300

    Created LoginView

--- program_manager/urls.py
+++ program_manager/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 import ProgramManager.views as views
 
 urlpatterns = [
     path('', views.index),
+    path('login/', auth_views.LoginView.as_view(extra_context=views.get_base_context('Вход')), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    path('projects/', views.projects),
+    path('project/<int:project_id>/', views.version),
+    path('project/<int:project_id>/version/<int:version_id>', views.version),
 ]
