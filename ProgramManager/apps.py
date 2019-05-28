960d0231b9e76a5fc97c971df586d774142ba12f
Andrey Popov <a.popov99@mail.ru>
2019-05-28 17:17:52 +0300

    Created admin auto creation

--- ProgramManager/apps.py
+++ ProgramManager/apps.py
@@ -3,3 +3,11 @@ from django.apps import AppConfig
 
 class ProgrammanagerConfig(AppConfig):
     name = 'ProgramManager'
+
+    def ready(self):
+        from django.contrib.auth.models import User
+        if User.objects.filter(is_superuser=True).count() == 0:
+            user = User.objects.create_superuser('admin', 'mail@mail.com', 'admin')
+            user.first_name = 'Администратор'
+            user.save()
+            print('Admin created')
